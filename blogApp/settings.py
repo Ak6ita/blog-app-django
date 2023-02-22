501136d52ba5a23497c44ea52ff8a24c3188c57b
Tamara <tamara.janakievska@yahoo.com>
2023-02-22 21:53:43 +0100

    Added localhost to allowed hosts

--- blogApp/settings.py
+++ blogApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-j@llg9oynn#u3dq755m(+($!ztgm&(gae1wpm8(86(15d=t2f1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ak6ita.github.io']
+ALLOWED_HOSTS = ['ak6ita.github.io', 'localhost']
 
 
 # Application definition
